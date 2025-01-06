067af7f4ddc99c1d8e19998ff6ab2e382ff288d2
Mihai Nadăș <mihai@nadas.ro>
2025-01-06 17:31:33 +0200

    Remove .env file and add configuration file for nbgpt CLI; implement command-line interface for improving and translating notebooks

--- nbgpt/core.py
+++ nbgpt/core.py
@@ -6,24 +6,53 @@ from openai import OpenAI
 import nbformat
 from tqdm import tqdm
 
-
-def configure_logging(level=logging.INFO):
-    """
-    Configures the logging module to display INFO level messages.
-    """
-    logging.basicConfig(
-        format="%(message)s",
-        level=logging.INFO,
-        handlers=[logging.StreamHandler(sys.stdout)],
-    )
+# Load environment variables from .env file
+load_dotenv()
 
 
-def get_logger():
+def get_logger(name=None, log_to_console=False, log_file="nbgpt.log"):
     """
     Configures and returns a logger instance.
+
+    Args:
+        name (str, optional): Name for the logger. Defaults to the module's __name__ if not provided.
+        log_to_console (bool): Whether to enable console logging. Defaults to False.
+        log_file (str): Path to the log file. Defaults to "app.log".
+
+    Returns:
+        logging.Logger: Configured logger instance.
     """
-    configure_logging()
-    return logging.getLogger(__name__)
+    # Use module name or the provided name for the logger
+    logger = logging.getLogger(name or __name__)
+
+    # Avoid duplicate handlers
+    if logger.hasHandlers():
+        return logger
+
+    # Determine log level based on DEBUG environment variable
+    debug_mode = os.getenv("DEBUG", "false").lower() == "true"
+    log_level = logging.DEBUG if debug_mode else logging.INFO
+    logger.setLevel(log_level)
+
+    # Create formatter
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    # Default: File handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(log_level)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # Optional: Console handler
+    if log_to_console:
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(log_level)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    return logger
 
 
 # Initialize logger
@@ -34,13 +63,13 @@ def check_required_vars():
     """
     Checks if the required variables are set.
     """
-    load_dotenv()
     required_vars = ["OPENAI_API_KEY"]
     for var in required_vars:
         if not os.getenv(var):
             raise EnvironmentError(f"Required environment variable {var} is missing.")
 
 
+# Check required environment variables
 check_required_vars()
 
 
@@ -53,30 +82,6 @@ def get_openai_client():
 
 # Initialize OpenAI client
 client = get_openai_client()
-
-
-def call_openai_llm(*llm_user_prompt):
-    """
-    Calls the OpenAI language model with the provided user prompt.
-    """
-    model = "gpt-4o"
-    llm_prompt_system = """
-    You are a world class, academic peer reviewer that acts as a helpful assistant for improving Jupyter notebooks developed for young students, containing both code and markdown cells.
-    The resulting notebook should be more readable, maintainable, and efficient. You are expected to provide constructive feedback and suggestions for improvement.
-    You can also suggest additional code snippets, explanations, or visualizations to enhance the notebook. Please ensure that the notebook is still functional after your changes.
-    """
-    messages = [
-        {"role": "system", "content": llm_prompt_system},
-        {"role": "user", "content": "\n".join(llm_user_prompt)},
-    ]
-    try:
-        completion = client.chat.completions.create(
-            model=model, messages=messages, max_tokens=1500
-        )
-        return completion.choices[0].message.content
-    except Exception as e:
-        logger.error(f"An error occurred while calling OpenAI LLM: {e}")
-        raise
 
 
 def get_nbcell_list(
@@ -118,81 +123,97 @@ def get_nbcell_list(
     return cells_list
 
 
-def generate_analysis(nbcell_list):
+def call_openai_llm(system_prompt, user_prompt, message, model="gpt-4o"):
+    """
+    Calls the OpenAI language model with the provided user prompt.
+    """
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": "\n".join([user_prompt, message])},
+    ]
+    try:
+        completion = client.chat.completions.create(
+            model=model, messages=messages, max_tokens=1500
+        )
+        return completion.choices[0].message.content
+    except Exception as e:
+        logger.error(f"An error occurred while calling OpenAI LLM: {e}")
+        raise
+
+
+def generate_analysis(system_prompt, user_prompt, nbcell_list):
     """
     Generates an analysis of the notebook cells using OpenAI LLM.
     """
     logger.info("Generating analysis for notebook cells.")
-    llm_prompt = "Analyze the following Python notebook and suggest improvements that would take it to the next level:"
     try:
-        combined_content = "\n".join(cell["source"] for cell in nbcell_list)
-        analysis = call_openai_llm(llm_prompt, combined_content)
-        logger.info("Analysis generated successfully.")
+        message = "\n".join([cell["source"] for cell in nbcell_list])
+        analysis = call_openai_llm(system_prompt, user_prompt, message)
+        logger.info(f"Analysis generated successfully.\n{analysis}\n")
         return analysis
     except Exception as e:
         logger.error(f"An error occurred during analysis: {e}")
         raise
 
 
-def generate_improved_nbcell(nbcell_list, llm_analysis, nbcell):
+def generate_improved_nbcell(
+    system_prompt, user_prompt, nbcell_list, llm_analysis, nbcell
+):
     """
     Generates an improved version of a notebook cell based on LLM analysis.
     """
-    llm_prompt = """
-    Significantly adjust the following Python notebook CELL, part of the ORIGINAL_NOTEBOOK, following the improvements suggested by the ANALYSIS.
-    Make sure that you strictly reply with the adjusted content only, *without any leading markers*.
-    When using LaTeX, you must only use single dollar signs ($) to wrap the LaTeX content.
-    """
     try:
-        improved_cell = call_openai_llm(
-            llm_prompt,
-            f"ORIGINAL_NOTEBOOK: {nbcell_list}",
-            f"ANALYSIS: {llm_analysis}",
-            f"CELL: {nbcell}",
+        message = "\n".join(
+            [
+                f"ORIGINAL_NOTEBOOK: {nbcell_list}",
+                f"ANALYSIS: {llm_analysis}",
+                f"CELL: {nbcell}",
+            ]
         )
+        improved_cell = call_openai_llm(system_prompt, user_prompt, message)
         return improved_cell
     except Exception as e:
         logger.error(f"An error occurred during improvement: {e}")
         raise
 
 
-def generate_translated_nbcell(nbcell_list, language, nbcell):
+def generate_translated_nbcell(
+    system_prompt, user_prompt, nbcell_list, language, nbcell
+):
     """
     Translates a notebook cell to the specified language using OpenAI LLM.
     """
-    llm_prompt = """
-    Translate the following Python notebook CELL to the specified TARGET_LANGUAGE.
-    Make sure that you strictly reply with the translated content only, *without any leading markers* (such as ```markdown or ```python).
-    When using LaTeX, you must only use single dollar signs ($) to wrap the LaTeX content.
-    If the cell contains code, strictly translate the comments only, without modifying the code itself.
-    """
     try:
-        translated_cell = call_openai_llm(
-            llm_prompt,
-            f"ORIGINAL: {nbcell_list}",
-            f"TARGET_LANGUAGE: {language}",
-            f"CELL: {nbcell}",
+        message = "\n".join(
+            [
+                f"ORIGINAL: {nbcell_list}",
+                f"TARGET_LANGUAGE: {language}",
+                f"CELL: {nbcell}",
+            ]
         )
+        translated_cell = call_openai_llm(system_prompt, user_prompt, message)
         return translated_cell
     except Exception as e:
         logger.error(f"An error occurred during translation: {e}")
         raise
 
 
-def generate_improved_nb(nb_path, cell_types=["markdown"]):
+def generate_improved_nb(
+    system_prompt, analysis_prompt, improvement_prompt, nb_path, cell_types=["markdown"]
+):
     """
     Generates an improved version of the notebook by iterating through its cells.
     """
     logger.info(f"Generating improved notebook for '{nb_path}'")
     nbcell_list = get_nbcell_list(nb_path)
-    llm_analysis = generate_analysis(nbcell_list)
+    llm_analysis = generate_analysis(system_prompt, analysis_prompt, nbcell_list)
     nbcell_list_improved = []
     for cell in tqdm(nbcell_list, desc="Generating iterative improvements"):
         if cell["type"] not in cell_types:
             nbcell_list_improved.append(cell)
             continue
         improved_cell = generate_improved_nbcell(
-            nbcell_list, llm_analysis, cell["source"]
+            system_prompt, improvement_prompt, nbcell_list, llm_analysis, cell["source"]
         )
         new_cell = cell.copy()
         new_cell["source"] = improved_cell
@@ -201,7 +222,13 @@ def generate_improved_nb(nb_path, cell_types=["markdown"]):
     return nbcell_list_improved
 
 
-def generate_translated_nb(nb_path, language="ro", cell_types=["markdown", "code"]):
+def generate_translated_nb(
+    system_prompt,
+    translation_prompt,
+    nb_path,
+    language="ro",
+    cell_types=["markdown", "code"],
+):
     """
     Generates a translated version of the notebook by iterating through its cells.
     """
@@ -213,7 +240,7 @@ def generate_translated_nb(nb_path, language="ro", cell_types=["markdown", "code
             nbcell_list_translated.append(cell)
             continue
         translated_cell = generate_translated_nbcell(
-            nbcell_list, language, cell["source"]
+            system_prompt, translation_prompt, nbcell_list, language, cell["source"]
         )
         new_cell = cell.copy()
         new_cell["source"] = translated_cell
@@ -239,17 +266,3 @@ def save_new_nb(nbcell_list, output_path):
     except Exception as e:
         logger.error(f"An error occurred while saving the new notebook: {e}")
         raise
-
-
-# # Paths for the original, improved, and translated notebooks
-# nb_path = "perceptron.ipynb"
-# nb_improved_path = nb_path.replace(".ipynb", "_improved.ipynb")
-# nb_translated_path = nb_path.replace(".ipynb", "_translated.ipynb")
-
-# # Generate improved notebook
-# nbcell_list_improved = generate_improved_nb(nb_path)
-# save_new_nb(nbcell_list_improved, nb_improved_path)
-
-# # Generate translated notebook
-# nbcell_list_translated = generate_translated_nb(nb_improved_path)
-# save_new_nb(nbcell_list_translated, nb_translated_path)
--- nbgpt/nbgpt_cli.py
+++ nbgpt/nbgpt_cli.py
@@ -1,59 +1,113 @@
-from core import get_logger
+from core import get_logger, generate_improved_nb, generate_translated_nb, save_new_nb
 import yaml
 import argparse
+import sys
+from datetime import datetime
 
+# Initialize logger
 logger = get_logger()
 
 
-def get_default_config():
-    llm_system_prompt = """
-        The following is a list of the most common programming languages:
-        """
-    llm_user_prompt = """
-        List the most common programming languages.
-        """
-
-    return {
-        "llm_system_prompt": llm_system_prompt,
-        "llm_user_prompt": llm_user_prompt,
-    }
-
-
+# Load configuration from a YAML file
 def load_config(file_path="config.yml"):
     try:
         with open(file_path, "r") as file:
             config = yaml.safe_load(file)
-            logger.info("Configuration loaded from file:")
+            logger.info(f"Configuration loaded from file: {file_path}")
+            logger.debug(f"Configuration: {config}")
             return config
     except FileNotFoundError:
         logger.error("Configuration file not found. Using default values.")
     except yaml.YAMLError as exc:
         print(f"Error parsing YAML file: {exc}. Using default values.")
-    return get_default_config()
+        raise
 
 
+# Retrieve a value from a nested dictionary using a list of keys
+def get_config_value(config, *keys):
+    """
+    Retrieve a value from a nested dictionary using a list of keys.
+
+    :param config: The configuration dictionary.
+    :param keys: A list of keys representing the path to the desired value.
+    :return: The value from the configuration dictionary.
+    :raises KeyError: If any key in the path is not found.
+    """
+    try:
+        value = config
+        for key in keys:
+            value = value[key]
+        return value
+    except KeyError as e:
+        logger.error(f"Key not found in configuration: {e}")
+        raise
+
+
+# Parse command-line arguments
 def parse_arguments():
     parser = argparse.ArgumentParser(description="CLI for nbgpt tasks")
     parser.add_argument(
         "--config", default="config.yml", help="Path to the configuration file"
     )
     parser.add_argument(
-        "--command", choices=["improve", "translate"], help="Command to execute"
+        "--task",
+        choices=["improve", "translate"],
+        default="improve",
+        help="Command to execute",
     )
     parser.add_argument(
-        "--language", default="ro", help="Target language for translation"
+        "--language", default="romanian", help="Target language for translation"
     )
+    parser.add_argument("nb_path", help="Path to the notebook file to process")
     return parser.parse_args()
 
 
+# Generate a new notebook path with a suffix and timestamp
+def get_new_nb_path(nb_path, suffix):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    return nb_path.replace(".ipynb", f"_{suffix}_{timestamp}.ipynb")
+
+
+# Create an improved version of the notebook
+def make_improved_nb(config, nb_path):
+    system_prompt = get_config_value(config, "system_prompt")
+    analysis_prompt = get_config_value(config, "improve", "prompts", "analysis")
+    improvement_prompt = get_config_value(config, "improve", "prompts", "improvement")
+    improved_nb = generate_improved_nb(
+        system_prompt, analysis_prompt, improvement_prompt, nb_path
+    )
+    new_nb_path = get_new_nb_path(nb_path, "improved")
+    save_new_nb(improved_nb, new_nb_path)
+    logger.info(f"Saving improved notebook to '{new_nb_path}'")
+
+
+# Create a translated version of the notebook
+def make_translated_nb(config, language, nb_path):
+    system_prompt = get_config_value(config, "system_prompt")
+    translation_prompt = get_config_value(config, "translate", "prompt")
+    translated_nb = generate_translated_nb(
+        system_prompt, translation_prompt, nb_path, language
+    )
+    new_nb_path = get_new_nb_path(nb_path, f"translated_{language.lower()}")
+    save_new_nb(translated_nb, new_nb_path)
+    logger.info(f"Saving translated notebook to '{new_nb_path}'")
+
+
+# Main function to execute the appropriate task based on arguments
 def main():
     args = parse_arguments()
     config = load_config(args.config)  # uses selected config file
-    if args.command == "improve":
-        print("Running improve command...")
-    elif args.command == "translate":
-        print(f"Running translate command in {args.language}...")
+    if args.task == "improve":
+        make_improved_nb(config, args.nb_path)
+    elif args.task == "translate":
+        language = args.language.capitalize()
+        make_translated_nb(config, language, args.nb_path)
 
 
+# Entry point of the script
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"An error occurred: {e}.")
+        sys.exit(1)
